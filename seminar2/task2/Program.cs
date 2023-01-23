ef3b2c17f9a8de75ab1f40f95b7e1ce95f629c88
Sem0Vektor <mozziella@proton.me>
2023-01-23 17:46:42 +0500

    task2

---
+++ seminar2/task2/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+1. Напишите программу, которая принимает на вход
+трёхзначное число и на выходе показывает последнюю 
+цифру этого числа.
+
+Например:
+        456 -> 6
+        782 -> 2
+        918 -> 8
+*/
+
+Console.Write("Enter number ");
+int n = int.Parse(Console.ReadLine()!);
+
+int lastDigit = 0;
+
+if(n > 0) lastDigit = n % 10;
+
+else lastDigit = n*(-1)%10;
+
+Console.Write($"Last digit of this number is {lastDigit}");
