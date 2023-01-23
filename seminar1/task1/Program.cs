4a250132e9c178f43ff08ad82ac6d9cb0bd7c14c
Sem0Vektor <mozziella@proton.me>
2023-01-23 15:44:26 +0500

    seminar1

---
+++ seminar1/task1/Program.cs
@@ -0,0 +1,21 @@
+﻿//'ReadLine' only reads string types -
+string input;
+input = Console.ReadLine()!;
+Console.WriteLine(input);
+//how to read integer number, here are two types:
+int number;
+number = int.Parse(Console.ReadLine()!);
+number = Convert.ToInt32(Console.ReadLine());
+
+int squareOfNumber = number * number;
+//you can add something to output in 'WriteLine' by '+':
+Console.WriteLine("squared number is " + squareOfNumber);
+//you can add something inside of quotes by '$' before them:
+Console.WriteLine($"Square of {number} = {squareOfNumber}");
+
+//maximum value of 'int32' is 'int.MaxValue', 'MinValue' is the opposite:
+Console.WriteLine(int.MaxValue);
+Console.WriteLine(int.MinValue);
+
+//you can make equations inside of brackets of 'WriteLine':
+Console.WriteLine(number + number);
