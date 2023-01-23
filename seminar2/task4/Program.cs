95cee555d9ddbc18197b819b5f2bcf1705949d1f
Sem0Vektor <mozziella@proton.me>
2023-01-23 18:41:38 +0500

    task4

---
+++ seminar2/task4/Program.cs
@@ -0,0 +1,16 @@
+﻿bool func()
+{
+    Console.WriteLine("func");
+    return true;
+}
+
+int x = 0;
+
+if(x >= 50 && func())
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
